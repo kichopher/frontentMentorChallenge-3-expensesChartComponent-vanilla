aa490bf5db862c18f863015e960bcfde1788b867
CHRISTOPHER <kichoraphael@gmail.com>
2022-08-19 01:21:58 +0530

    modify spacings

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ function insertBars(data) {
         const bar = `
         <div class="bar-chart-bar">
             <div class="bar__flag">$${dataObj.amount}</div>
-            <div class="bar ${dataObj.amount === maxAmount? 'bar--max': ''}" style="height: ${dataObj.amount * 0.15}rem;"></div>
+            <div class="bar ${dataObj.amount === maxAmount? 'bar--max': ''}" style="height: ${dataObj.amount * 0.175}rem;"></div>
             <p class="label">${dataObj.day}</p>
         </div>
         `
         barChartContainer.insertAdjacentHTML("beforeend", bar)
     })
-
+    
     const bars = document.querySelectorAll(".bar-chart-bar>.bar")
     bars.forEach(bar=>{
         bar.addEventListener("mouseenter", (event)=>{
